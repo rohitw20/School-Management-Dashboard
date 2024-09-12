fee87f6f051feffea64eb49f2b7fa1b6dd2eb16c
rohitw20 <rohit.w@aikyamedge.com>
2024-09-12 12:24:48 +0530

    added menu and navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -112,3 +112,33 @@ const menuItems = [
     ],
   },
 ];
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm ">
+      {menuItems.map((item) => (
+        <div className="flex flex-col gap-2" key={item.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {item.title}
+          </span>
+          {item.items.map((i) => (
+            <Link
+              href={i.href}
+              key={i.label}
+              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+            >
+              <Image src={i.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{i.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
