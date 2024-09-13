66af884b0c0602b686c4b02e4b5358a9c8227ffd
rohitw20 <rohit.w@aikyamedge.com>
2024-09-13 16:18:08 +0530

    parent and teacher pages

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,7 +1,24 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
 import React from "react";
 
 const Teacher = () => {
-  return <div>Teacher</div>;
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+
+      {/* right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default Teacher;
