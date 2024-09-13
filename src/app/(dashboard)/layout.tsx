edde99163e074a9982e55a1d9a9bb5a40bf38864
rohitw20 <rohit.w@aikyamedge.com>
2024-09-13 12:58:24 +0530

    added announcements and event calender

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ export default function DashboardLayout({
           className="flex items-center justify-center lg:justify-start gap-2"
         >
           <Image src={"/logo.png"} alt="logo" width={32} height={32} />
-          <span className="hidden lg:block">MySchoool</span>
+          <span className="hidden lg:block font-bold">MySchoool</span>
         </Link>
         <Menu />
       </div>
